0adecb48be6e286614757d7f9bdbc59db1e85442
bdurb <brian@rpmgreenville.com>
2018-08-26 18:01:37 -0700

    added Router to index.js, set up basic route to reviews, and boilerplate layout for ReviewList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Route } from 'react-router-dom';
+import ReviewList from './components/ReviewList';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       Hello
+      <div>
+       <Route exact path ='/' component={ReviewList} />
       </div>
     );
   }
---
+++ src/components/ReviewList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ReviewList extends Component {
+  state = {
+    results: []
+  }
+  render() { 
+    return (
+      <div>
+        placeholder
+      </div>
+    );
+  }
+}
+ 
+export default ReviewList;
